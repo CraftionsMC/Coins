fbea5c4e970b59512c7a001ddb942e23ab512dab
MCTzOCK <dev@mctzock.de>
2021-05-25 10:59:42 +0200

    Added basic files

---
+++ coinsystem-common/src/main/java/net/craftions/coinsystem/common/Coins.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.coinsystem.common;
+
+public class Coins {
+}
---
+++ coinsystem-waterfall/src/main/java/net/craftions/coinsystem/env/waterfall/Coinsystem.java
@@ -0,0 +1,17 @@
+package net.craftions.coinsystem.env.waterfall;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Coinsystem extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
